f264c2a5a96fdaae2a80a606287e7d88fd371dc2
Manish Mahi <manishmahi505@gmail.com>
2026-02-06 22:25:16 +0530

    Update footer with version comment to trigger build

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 const Footer = () => {
+    // v1.1 - Updated Compact Design
     const theme = useTheme();
     return (
         <Box component="footer" sx={{
